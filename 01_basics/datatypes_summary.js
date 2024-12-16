cb7a6b3d0f59b2956058ff55576d439d2f09a0c0
ABHISHEK PATIL <80521018+abhishekpatil1289@users.noreply.github.com>
2024-12-16 09:17:37 +0000

    Stack and Heap Understanding

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -25,7 +25,39 @@ let bud = {
 
 const fun = function()
 {
-    console.log("hello world");
+    // console.log("hello world");
 }
 
-console.log(fun);
+// console.log(fun);
+
+
+
+// primitive data type Examples 
+
+let a = 5;
+let b = a ;
+b = 7 ;
+// console.log(b)
+
+/*
+In the above example i provide the a value to the b 
+and again i change the value of "b" which dosent effect on "a" value because its a primetive datatype which always refers the copy to the anohter variable 
+// [ String, Number, Boolean, Undefine, Null, Symbol, BigInt ] this are stored in Stack Memory
+Stack memory Provide Copy and Heap Provide Refrence always keep in mind
+*/
+
+
+let ab = {
+    name : "buds",
+    size : 4
+}
+
+let bc = ab;
+bc.name = "onePlus";
+
+console.log(bc)
+/*
+In the above example i provide the "ab" value to the "bc" 
+and again i change the value of "bc.name" then it also effects on "ab" object also because, Object is Non-Preative DataType which directly gives the refrenct from the Heam Memory 
+// Array , Function, Object are stred in Heap Function 
+*/
