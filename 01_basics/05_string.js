4565b7976621d07b985329a6bc262a33ae966986
ABHISHEK PATIL <80521018+abhishekpatil1289@users.noreply.github.com>
2024-12-17 06:04:57 +0000

    01_Array done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,13 @@
+let a = new String("hello");
+
+
+// let a = "Abhishek";
+// let holder = '';
+// for (let i = a.length - 1; i >= 0; i--) {
+//     holder = holder + a[i]; 
+// }
+// console.log(a);
+// console.log(holder);
+
+a = "abhi"
+console.log(a)
