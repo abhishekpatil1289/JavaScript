fd7d9596bb966f93fb3960c62c203d639d9126bc
ABHISHEK PATIL <80521018+abhishekpatil1289@users.noreply.github.com>
2024-12-17 09:13:18 +0000

    02_Array done

---
+++ 02_Basics/01_Array copy.js
@@ -0,0 +1,38 @@
+const Array = [0, 1, 2, 3, 4, 5, 6, true, "hitest"];
+
+// console.log(Array)
+
+// Array.push(10);
+
+// console.log(Array)
+
+// Array.pop()
+// console.log(Array)
+
+// Array.unshift(8);
+// console.log(Array)
+
+// Array.pop()
+
+
+
+// console.log(Array.includes(9));
+// console.log(Array.unshift(9));
+
+// console.log(Array)
+
+// console.log(Array.indexOf(5))
+
+// const newArray = Array.join(); // convert all the array elements into the string without changing its original value
+
+// console.log(typeof Array);
+// console.log(newArray);
+
+
+const temp = Array.slice(1, 3);
+console.log("original array A ",Array);
+
+
+const temp1 = Array.splice(1, 4); // splice prototype cut the values from original array and remove that range of value from array
+console.log("original array B ",Array);
+
---
+++ 02_Basics/02_Array.js
@@ -0,0 +1,38 @@
+const marvel_Heros = ["IronMan", "spiderman", "thor"];
+
+const dc = ["superman", "batman", "flash"];
+
+
+// marvel_Heros.push(dc);
+// console.log(marvel_Heros);
+
+// const newArray =marvel_Heros.concat(dc);
+// console.log(newArray);
+
+// const ab=[];
+
+// ab.push(...dc);
+// console.log(ab);
+
+
+const duplicate = [1, 2, 3, [9, 7, [5, 7], [8, 5, 3]], [5, 6, 7]];
+
+// console.log(duplicate.flat(Infinity)); // infinity is used for check the depth of array 
+
+// console.log(Array.from("abhishek"));
+
+// let arrat = [1,2,3];
+
+// let holder  = arrat.join();
+
+// const hold = str.split().reverse().join();
+// const hold = Array.from("sky is the limit");
+
+// console.log(hold);
+
+
+let score1 = 200;
+let score2 = 300;
+let score3 = 400;
+
+console.log(Array.of(score1, score2, score3))
