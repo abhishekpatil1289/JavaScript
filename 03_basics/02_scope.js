080104ad16d43c90ea05405395a67489ea79866b
ABHISHEK PATIL <80521018+abhishekpatil1289@users.noreply.github.com>
2024-12-18 13:01:27 +0000

    hoisting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -2,11 +2,27 @@ let a = 10;
 const b = 20;
 var c = 30;
 
-if (true) { 
+if (true) {
     c = 500;
-    console.log("condition scope",c)
+    // console.log("condition scope", c)
 }
 
-console.log(a)
-console.log(b)
-console.log(c)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+//Hoisting
+
+Game(4)
+function Game(num) {
+    console.log("GameZone")
+    return num;
+}
+
+
+// holder(5) // it gives error because its not access before its decleration where Game() can be accessable
+const holder = (num) => {
+    console.log("hello from holder")
+    return num;
+}
